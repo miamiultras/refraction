3266803262b61446431a207331b7f949cb2a7678
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-06-26 10:55:20 +0200

    setup ValidationPipe globally

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Refraction')
