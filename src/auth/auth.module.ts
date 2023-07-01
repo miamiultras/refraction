702723017822f0507667ac2b733778197bd3ddf1
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-07-01 10:47:54 +0200

    install and configure passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+export const jwtSecret = 'I5LoSKCRj6Z2LoSGIEQGJKDABDKJhd4y82h3urfewkjsfn';
 
 @Module({
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: '5m' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
