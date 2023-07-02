575a22271e6b6b5ebed76adba08b8c70cfdef872
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-07-02 12:42:32 +0200

    implement JWT authentication strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersModule } from 'src/users/users.module';
+import { JwtStrategy } from './jwt.strategy';
 
 export const jwtSecret = 'I5LoSKCRj6Z2LoSGIEQGJKDABDKJhd4y82h3urfewkjsfn';
 
@@ -15,8 +17,9 @@ export const jwtSecret = 'I5LoSKCRj6Z2LoSGIEQGJKDABDKJhd4y82h3urfewkjsfn';
       secret: jwtSecret,
       signOptions: { expiresIn: '5m' },
     }),
+    UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
